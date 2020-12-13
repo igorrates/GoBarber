2c18004ba17f49ad1bb433441ab8be297baf18af
Igor Rates <igor.rates@gmaill.com>
2020-12-13 16:09:09 -0300

    Navigation done

--- appgobarber/src/pages/SignUp/index.tsx
+++ appgobarber/src/pages/SignUp/index.tsx
@@ -1,11 +1,60 @@
 import React from 'react';
+import {
+  Image,
+  View,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
 const SignUp: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
-    <Container />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Create Account</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Name" />
+
+            <Input name="email" icon="mail" placeholder="Email" />
+
+            <Input name="password" icon="lock" placeholder="Password" />
+
+            <Button onPress={() => console.log('Worked')}>Create</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+
+        <BackToSignInText>Back to logon</BackToSignInText>
+      </BackToSignIn>
+    </>
   );
-}
+};
 
 export default SignUp;
